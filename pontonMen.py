ea320e421da142349fb3c1f279b1a7eee55f3772
William Ponton <waponton@gmail.com>
2019-01-05 23:28:38 -0500

    Add files via upload

---
+++ pontonMen.py
@@ -0,0 +1,93 @@
+initials = {"KKP" : "Kyle Kenneth Ponton",      #Kenneth
+		    "JMP" : "James Marcus Ponton",      #Mark
+		    "KWP" : "Kenneth Wayne Ponton",     #Wayne
+		    "WAP" : "William Andrew Ponton",    #Will
+		    "KAP" : "Kenneth Anthony Ponton",   #Ken
+		    "CJP" : "Carson James Ponton"       #Carson"}
+		    }
+
+
+
+
+print("*******************************\n")
+print("*******************************\n")
+print("Welcome to the Ponton Man Test.\n")
+print("*******************************\n")
+print("*******************************\n")
+print("*******************************\n")
+	
+while True:	
+	in1 = str(input("What are your initials?"))
+	init = in1.upper()
+	print(init, "-", "Nice initials! \n Let\'s see if you are a TRUE Ponton Man!!!\n\n")
+	print("*******************************\n")
+	if in1 == "":
+		break
+	
+	if init in initials:
+		print("YES,", init, "!\n", initials[init], "is your name, and you are a TRUE Ponton Man!!!\n\n")
+		
+	else:
+		print("You are not a True Ponton man!")
+
+#init =(init.upper()
+#print(init)
+#print("Nice initials!  Let\'s see if you are a TRUE Ponton Man!!!")
+
+
+#Identifier
+
+
+
+
+
+
+
+
+
+
+
+
+# if init == key in pont == "kkp" or key in pont == "KKP":
+	# print("YES! \nYou are the ultimate badass, Paw Paw!!!")
+# elif key in pont == "jmp" or key in pont == "JMP":
+	# print("YES! \nYou are the most intelligent Ponton, Mark!!!")
+# elif key in pont == "kwp" or key in pont == "KWP":
+	# print("YES! \nYou are the nicest Ponton, Uncle Wayne!!!")
+# elif key in pont == "wap" or key in pont == "WAP":
+	# print("YES! \nYou are the funniest Ponton, William!!!")
+# elif key in pont == "kap" or key in pont == "KAP":
+	# print("YES! \nYou are the newest Ponton dad, Kenny!!!")
+# elif key in pont == "cjp" or key in pont == "CJP":
+	# print("YES! \nYou are the newest Ponton man, Carson!!!")
+# else:
+	# print("NO! \nYou are not a Ponton, sorry your existence is absurd.")
+	# print("WOW... \nYou are not a Ponton, \nplease stop entering irrelavent and non-historical initials, you are wasting precious time of many important Texans!!!")
+
+	
+# # #Comparisons
+# # if pont < "kkp" or pont < "KKP":
+		# # print("SORRY! \nYou will never be as tough as Paw Paw!!!")
+# # if pont < "jmp" or pont < "JMP":
+		# # print("SORRY! \nYou will never be as smart as Mark!")
+# # if pont < "kwp" or pont < "KWP":
+		# # print("SORRY! \nYou will never be as nice as Wayne!!!")
+# # if pont < "wap" or pont < "WAP":
+		# # print("SORRY! \nYou will never be as hilarious as William!!!")
+# # if pont < "kap" or pont < "KAP":
+		# # print("SORRY! \nYou will never be as virile as Kenny!!!")
+# # if pont < "cjp" or pont < "CJP":
+		# # print("SORRY! \nYou will never be as young as Carson is again!!!")
+# # else:
+	# # print("NO! \nYou are not a Ponton, sorry your existence is absurd.")
+	# print("WOW... \nYou are not a Ponton, \nplease stop entering irrelavent and non-historical initials, you are wasting precious time of many important Texans!!!")
+
+
+	
+	
+
+
+
+
+
+
